5001d908f44ef75635bc883a96169593e7f388e1
Jack Dreifus <j.dreifus@northeastern.edu>
2025-10-13 16:01:10 -0400

    remove songs subprocess

--- backend/run.py
+++ backend/run.py
@@ -1,12 +1,7 @@
 import os
-import subprocess
-import sys
 import uvicorn
 
 if __name__ == "__main__":
-  subprocess.run([sys.executable, "-m", "src.chroma.seed_songs", "--reset"])
-
-
   uvicorn.run(
     "src.main:app",
     host="0.0.0.0",
